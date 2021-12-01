ef3a671db62dc4a4e7cdb8199e102833191e83ab
Chetan Malvia <53286038+cmalvia@users.noreply.github.com>
2021-12-01 19:12:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hell Paloalto!"));
+app.get("/", (req, res) => res.send("Hell Roshan!!!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
